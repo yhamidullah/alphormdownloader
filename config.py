916dea9bdcd9242681d7f136b8d174e1f2f21a6e
HAMIDULLAH Yasser <yasserhamidullah@gmail.com>
2020-04-05 22:11:36 +0700

    bug fixed not clicked

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ email = "vietnampromo23@gmail.com"
 password = "8champion9"
 #course list
 courses = [
-    "https://www.alphorm.com/tutoriel/formation-en-ligne-excel-2016-niveau-expert-2-2","https://www.alphorm.com/tutoriel/formation-en-ligne-excel-2016-niveau-expert-2-2",
+    "https://www.alphorm.com/tutoriel/formation-en-ligne-adobe-illustrator-cc-2019-creer-et-vendre-des-t-shirts-sur-teespring-avec-facebook","https://www.alphorm.com/tutoriel/formation-en-ligne-excel-2016-niveau-expert-2-2",
     "https://www.alphorm.com/tutoriel/formation-en-ligne-powerpoint-2016-perfectionnement",
 ]
